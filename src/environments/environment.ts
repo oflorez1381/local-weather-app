2023b51996a159baa317d9efba1ba2555b32fbf5
oflorez <v-oflorez@expedia.com>
2020-06-30 22:33:13 -0400

    chapter 03

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  appId: '7f0815aa8de899af357da9a6fa0283ec',
+  openWeatherUrl: 'http://api.openweathermap.org/data/2.5'
 };
 
 /*
