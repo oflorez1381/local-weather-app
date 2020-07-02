e58daf9208a2e4f54f19dd60ea3a399bd5a1d3b1
oflorez <v-oflorez@expedia.com>
2020-07-02 17:17:18 -0400

    chapter 04 - fixing test

--- src/app/weather/weather.service.ts
+++ src/app/weather/weather.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core'
 import { HttpClient, HttpParams } from '@angular/common/http'
 import { environment } from '../../environments/environment'
-import { ICurrentWeather } from '../interfaces';
+import { ICurrentWeather } from '../interfaces'
 import { map } from 'rxjs/operators'
+import { Observable } from 'rxjs'
 
 export interface ICurrentWeatherData {
   weather: [
@@ -21,29 +22,33 @@ export interface ICurrentWeatherData {
   name: string
 }
 
+export interface IWeatherService {
+  getCurrentWeather(city: string, country: string): Observable<ICurrentWeather>;
+}
+
 @Injectable({
   providedIn: 'root',
 })
-export class WeatherService {
+export class WeatherService implements IWeatherService {
   constructor(private httpClient: HttpClient) {}
 
-  getCurrentWeather(city: string, country: string) {
+  getCurrentWeather(city: string, country: string): Observable<ICurrentWeather> {
     const uriParams = new HttpParams()
       .set('q', `${city},${country}`)
-      .set('appid', environment.appId)
+      .set('appid', environment.appId);
 
-    return this.httpClient.get<ICurrentWeatherData>(
-      `${environment.openWeatherUrl}/weather`,
-      { params: uriParams }
-    )
-      .pipe(map((data) => this.transformToICurrentWeather(data)))
+    return this.httpClient
+      .get<ICurrentWeatherData>(`${environment.openWeatherUrl}/weather`, {
+        params: uriParams,
+      })
+      .pipe(map((data) => this.transformToICurrentWeather(data)));
   }
 
   private transformToICurrentWeather(data: ICurrentWeatherData): ICurrentWeather {
     return {
       city: data.name,
       country: data.sys.country,
-      date: data.dt * 1000,
+      date: new Date(data.dt * 1000),
       image: `http://openweathermap.org/img/w/${data.weather[0].icon}.png`,
       temperature: this.convertKelvinToFahrenheit(data.main.temp),
       description: data.weather[0].description,
